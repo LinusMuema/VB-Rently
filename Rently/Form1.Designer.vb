fe8ac452f24188de89b71718875b60a6b28cbcf2
Linus Muema <linus.m.muema@gmail.com>
2021-11-28 17:09:18 +0300

    Add project files.

---
+++ Rently/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Add_User = New System.Windows.Forms.Button()
+        Me.Add_Space = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Add_User
+        '
+        Me.Add_User.Location = New System.Drawing.Point(40, 39)
+        Me.Add_User.Name = "Add_User"
+        Me.Add_User.Size = New System.Drawing.Size(141, 52)
+        Me.Add_User.TabIndex = 0
+        Me.Add_User.Text = "Add User"
+        Me.Add_User.UseVisualStyleBackColor = True
+        '
+        'Add_Space
+        '
+        Me.Add_Space.Location = New System.Drawing.Point(352, 39)
+        Me.Add_Space.Name = "Add_Space"
+        Me.Add_Space.Size = New System.Drawing.Size(156, 52)
+        Me.Add_Space.TabIndex = 1
+        Me.Add_Space.Text = "Add Space"
+        Me.Add_Space.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Add_Space)
+        Me.Controls.Add(Me.Add_User)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Add_User As Button
+    Friend WithEvents Add_Space As Button
+End Class
---
+++ Rently/Form1.vb
@@ -0,0 +1,54 @@
+﻿Public Class Form1
+    Private cmd As New OleDb.OleDbCommand
+    Private da As New OleDb.OleDbDataAdapter
+    Private con As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\moose\Documents\Rently.accdb")
+
+    Private Sub Add_User_Click(sender As Object, e As EventArgs) Handles Add_User.Click
+        Try
+            Dim name = "test"
+            Dim balance = 15000
+            Dim email = "testing@mail.com"
+            Dim phone = "+254759458280"
+
+            con.Open()
+            cmd.Connection = con
+            cmd.CommandText = $"insert into users (fullname, email, phone, balance) values ('{name}', '{email}', '{phone}', '{balance}')"
+
+            Dim result = cmd.ExecuteNonQuery
+            If result > 0 Then
+                MsgBox($"{name} has been created successfully!")
+            Else
+                MsgBox("Oops! something happened. Try again later.")
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            con.Close()
+        End Try
+    End Sub
+
+    Private Sub Add_Space_Click(sender As Object, e As EventArgs) Handles Add_Space.Click
+        Try
+            Dim name = "H05"
+            Dim rent = 15000
+            Dim description = "another 2 b/r but this time, from the app..."
+
+            con.Open()
+            cmd.Connection = con
+            cmd.CommandText = $"insert into spaces (housename, rent, description) values ('{name}', '{rent}', '{description}')"
+
+            Dim result = cmd.ExecuteNonQuery
+            If result > 0 Then
+                MsgBox($"{name} has been created successfully!")
+            Else
+                MsgBox("Oops! something happened. Try again later.")
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            con.Close()
+        End Try
+    End Sub
+End Class
